058c70507f53ab3f41e70919e8cd3ac9f3ac54fa
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-12-16 10:07:50 -0700

    added db tables and associations

--- server/index.js
+++ server/index.js
@@ -3,12 +3,20 @@ const express = require('express')
 const cors = require('cors')
 const server = express()
 const db = require('./util/database')
+const {User, Product, Cart} = require('./util/models')
 
 
 // ---Middleware--- //
 server.use(express.json())
 server.use(cors())
 
+
+// ---Associations--- //
+User.hasMany(Cart)
+Cart.belongsTo(User)
+
+Product.hasMany(Cart)
+Cart.belongsTo(Product)
 
 // ---Endpoints--- //
 
