0279308f1090b634b2f3624a7e4f7e405e4b9da6
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-12-16 15:56:33 -0700

    added endpoints

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const server = express()
 const db = require('./util/database')
-const {User, Product, Cart} = require('./util/models')
+const { User, Product, Cart } = require('./util/models')
 
 
 // ---Middleware--- //
@@ -19,8 +19,25 @@ Product.hasMany(Cart)
 Cart.belongsTo(Product)
 
 // ---Endpoints--- //
+server.post('/api/addUser', async (req, res) => {
+    const { username, password } = req.body
+    try {
+        await User.create({ username, password })
+        res.status(200).send("User created!")
+    } catch {
+        res.status(401).send("Could not create user")
+    }
+})
 
-
+server.get('/api/user/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        let info = await User.findOne({where: {id: id}})
+        res.status(200).send(info)
+    } catch {
+        res.status(400).send("Couldn't complete request")
+    }
+})
 
 db.sync()
 // ---Listen--- //
